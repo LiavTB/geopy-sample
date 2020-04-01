92712a584446b39ed12edb061f602290771a6838
Liav Tibi <liavt11@gmail.com>
2020-04-01 21:51:42 +0300

    First commit for checks

--- main.py
+++ main.py
@@ -1,23 +1,21 @@
 # -*- coding: utf-8 -*-
+import math
 import pandas as pd
 import time
-from fuzzywuzzy import fuzz
+from geopy.distance import great_circle
 
 from models.GeoLocator import GeoLocator
-from TranslationHandler import translate_if_needed
+from geocoder_handlers import azure_handler, arcgis_handler
 
 
 def use_all_locators(geolocators, address):
-    locations = []
+    locator_locations = []
     for geolocator in geolocators:
         location = None
         retries_left = 6
         while retries_left != 0:
             try:
-                # if geolocator.api_key is not None:
-                #     location = geolocator.locator.geocode(query=address, api_key=geolocator.api_key)
-                # else:
-                location = geolocator.locator[0].geocode(query=address, exactly_one=False)
+                location = geolocator.locator.geocode(query=address, exactly_one=False, **geolocator.geocode_args)
                 retries_left = 0
             except Exception as e:
                 print(e)
@@ -26,67 +24,138 @@ def use_all_locators(geolocators, address):
                 retries_left = retries_left - 1
 
         if location is not None:
-            locations.append((geolocator.name, location))
-    return locations
-
-
-def is_english(s):
-    try:
-        s.encode('ascii')
-    except UnicodeEncodeError:
-        return False
-    else:
-        return True
-
-
-def choose_location(locations, original_address):
-    location_score = map(lambda location: (location, fuzz.token_sort_ratio(original_address, translate_if_needed(location.address))),
-                        locations)
-    result = max(location_score, key=lambda loc_scr: loc_scr[1])
-    return result[0]
+            locator_locations.append((geolocator, location))
+    return locator_locations
 
 
 if __name__ == '__main__':
     print("start_time: " + str(time.strftime("%I:%M:%S %p", time.localtime())))
 
-    geocoders_names = [("Nominatim", {}),
-                       ("Photon", {}),
-                       ("ArcGIS", {}),
-                       # TODO - SET api_key FOR "Bing" SERVICE (can be generated from Bing map service)
-                       ("Bing", {"api_key": "API_KEY_TO_SET"}),
-                       # TODO - SET username FOR "geonames" SERVICE (can be generated from geonames service)
-                       ("geonames", {"username": "USERNAME_TO_SET"})
-                       ]
+    geolocators = [
+        GeoLocator(geocoder_name="Nominatim"),
+        GeoLocator(geocoder_name="Photon"),
+        GeoLocator(geocoder_name="ArcGIS",
+                   geocode_args={"out_fields": "*"},
+                   choose_location_func=arcgis_handler.choose_location),
+        # # TODO - SET subscription_key FOR "azure" SERVICE (can be generated from azure map service)
+        GeoLocator(geocoder_name="azurefuzzy",
+                   settings={"subscription_key": "subscription_key"},
+                   geocode_args={'typeahead': True},
+                   choose_location_func=azure_handler.choose_location,
+                   get_classifications_func=azure_handler.get_classifications),
+        # # TODO - SET api_key FOR "Bing" SERVICE (can be generated from Bing map service)
+        GeoLocator(geocoder_name="Bing",
+                   settings={"api_key": "api_key"}),
+        # # TODO - SET username FOR "geonames" SERVICE (can be generated from geonames service)
+        GeoLocator(geocoder_name="geonames",
+                   settings={"username": "username"})
+    ]
 
-    geolocators = list(map(lambda name_settings: GeoLocator(name_settings[0], name_settings[1]), geocoders_names))
+    # CSV
+    # TODO - Set the file path to read from
+    # exposure_locations_original_csv = 'file path'
+    # output_csv_base = './output_wanted_result_example/test_result_arcgis{}.csv'
+    # service_output_csv = output_csv_base.format("")
+    # service_output_close_csv = output_csv_base.format("_close")
+    # location_col_name = 'מקום'
+    # wanted_result = pd.read_csv(exposure_locations_original_csv)
 
-    wanted_result = pd.read_csv('./output_wanted_result_example/exposure_locations.csv')
-    # wanted_result_shorted = wanted_result[['x', 'y', 'מקום']]
-    wanted_result_shorted = wanted_result
+    # Excel
+    # TODO - Set the file path to read from
+    exposure_locations_original_excel = 'file path'
+    output_csv_base = './big_data/test_result_azure{}.csv'
+    service_output_csv = output_csv_base.format("")
+    service_output_close_csv = output_csv_base.format("_try_try_close")
+    location_col_name = 'place'
+    wanted_result = pd.read_excel(exposure_locations_original_excel)
 
-    new_results = []
+    geocoder_x_suffix = '_x'
+    geocoder_y_suffix = '_y'
+    geocoder_classification = '_classification'
 
-    counter = 0
+    # # Use All the GeoLocators to find the locations
 
-    for i, row in wanted_result_shorted.iterrows():
-        counter = counter + 1
-        print('counter: ' + str(counter))
-        curr_address = row['מקום']
-        address_locations = use_all_locators(geolocators, curr_address)
-        print(str(time.strftime("%I:%M:%S %p", time.localtime())))
-        for locations in address_locations:
-            location = choose_location(locations[1], curr_address)
-            wanted_result_shorted.at[i, str(locations[0]) + '_address'] = location.address
-            wanted_result_shorted.at[i, str(locations[0]) + '_x'] = location.longitude
-            wanted_result_shorted.at[i, str(locations[0]) + '_y'] = location.latitude
+    for i, row in wanted_result.iterrows():
+        print('counter: ' + str(i + 1))
+        curr_address = row[location_col_name]
+        if str(curr_address) != 'nan':
+            locator_locations = use_all_locators(geolocators, curr_address)
+            print(str(time.strftime("%I:%M:%S %p", time.localtime())))
+            print('searching location for: ' + str(curr_address))
+            for locator_locations in locator_locations:
+                locator = locator_locations[0]
+                location = locator.choose_location(locator_locations[1], curr_address)
+                if location is not None:
+                    print('the location found is: ' + str(location.address))
+                    wanted_result.at[i, str(locator.name) + '_address'] = location.address
+                    wanted_result.at[i, str(locator.name) + geocoder_x_suffix] = location.longitude
+                    wanted_result.at[i, str(locator.name) + geocoder_y_suffix] = location.latitude
+                    wanted_result.at[i, str(locator.name) + geocoder_classification] = \
+                        '[' + ','.join(locator.get_classifications(location)) + ']'
         print()
 
     print("end_time: " + str(time.strftime("%I:%M:%S %p", time.localtime())))
-    wanted_result_shorted.to_csv('./output_wanted_result_example/test_result_5_wuzzy.csv')
+    wanted_result.to_csv(service_output_csv)
 
-    # address = "בית כנסת אבני חושן שכונת קייזר מודיעין"
-    # address2 = "קניון רחובות"
-    # address_locations = use_all_locators(geolocators, address)
-    # address2_locations = use_all_locators(geolocators, address2)
+    # # Add the CHECK FOR CLOSE LOCATIONS OF X and Y
+
+    # CSV
+    # org_x_suffix = 'x'
+    # org_y_suffix = 'y'
+
+    # Excel
+    org_x_suffix = 'POINT_X'
+    org_y_suffix = 'POINT_Y'
+
+    distance_suffix = '_distance'
+    close_suffix = '_close'
+
+    check_result = pd.read_csv(service_output_csv)
+    for i, row in check_result.iterrows():
+        counter = i
+        print('counter: ' + str(counter))
+        print(str(time.strftime("%I:%M:%S %p", time.localtime())))
+        for geolocator in geolocators:
+            name = str(geolocator.name)
+            if name + geocoder_x_suffix in row:
+                longitude = float(row[name + geocoder_x_suffix])
+                latitude = float(row[name + geocoder_y_suffix])
+                longitude_org = float(row[org_x_suffix])
+                latitude_org = float(row[org_y_suffix])
+                if math.isnan(longitude):
+                    check_result.at[i, name + close_suffix] = ''
+                else:
+                    point_found = (latitude, longitude)
+                    point_original = (latitude_org, longitude_org)
+                    distance = great_circle(point_original, point_found).meters
+
+                    check_result.at[i, name + distance_suffix] = distance
+                    check_result.at[i, name + close_suffix] = distance <= 50
+        print()
+    check_result.to_csv(service_output_close_csv)
+
+    # # SUM the CLOSED LOCATIONS
+    sum_true = 0
+    sum_true_2_plus = 0
+    check_result = pd.read_csv(service_output_close_csv)
+    for i, row in check_result.iterrows():
+        counter = i
+        print('counter: ' + str(counter))
+        print(str(time.strftime("%I:%M:%S %p", time.localtime())))
+        true_count = 0
+        for geolocator in geolocators:
+            name = str(geolocator.name) + close_suffix
+            if name in row:
+                close = str(row[name])
+                # if close == 'True' or close == 'False':
+                if close == 'True':
+                    true_count = true_count + 1
+        if true_count > 0:
+            sum_true = sum_true + 1
+        if true_count > 1:
+            sum_true_2_plus = sum_true_2_plus + 1
+
+    print('sum_true: ' + str(sum_true))
+    print('sum_true_2_or_more: ' + str(sum_true_2_plus))
 
     print("done")
--- models/GeoLocator.py
+++ models/GeoLocator.py
@@ -1,13 +1,63 @@
 from geopy import get_geocoder_for_service
+from geopy.exc import GeocoderNotFound
+from custom_geocoders import get_custom_geocoder
+from geocoder_handlers import general_handler
 
 
 class GeoLocator:
-    def __init__(self, name, settings=None):
+    def __init__(self, geocoder_name, settings=None, name=None, geocode_args=None,
+                 choose_location_func=general_handler.choose_location,
+                 get_classifications_func=general_handler.get_classifications):
+        """
+        :param geocoder_name: te geocoder name to get and initialize in the GeoLocator
+        :type geocoder_name: str
+        :param settings: arguments for the geocoder initialization
+        :type settings: dict
+        :param name: custom name for the GeoLocator, If not set using the geocoder_name
+        :type name: str
+        :param geocode_args: arguments for the geocoder.geocode method
+        :type geocode_args: dict
+        :param choose_location_func: function to use for choosing from locations list
+                See for Example `geocoder_handlers.general_handler.choose_location`.
+        :param get_classifications_func: function to use to get the classification of location
+                See for Example `geocoder_handlers.general_handler.get_classification`.
+        """
         if settings is None:
             settings = {}
-        self.name = name,
-        self.locator = get_geo_locator(name, settings),
+        if geocode_args is None:
+            self.geocode_args = {}
+        else:
+            self.geocode_args = geocode_args
+        if name is None:
+            self.name = geocoder_name
+        else:
+            self.name = name
+        self.locator = get_geo_coder(geocoder_name, settings)
+
+        self.choose_location = choose_location_func
+
+        self.get_classifications = get_classifications_func
 
 
-def get_geo_locator(name, settings):
-    return get_geocoder_for_service(name)(user_agent='locator_tester', timeout=6, **settings)
+def get_geo_coder(service_name, settings):
+    """
+            Return a geocoder.
+            geocoder is based on the service_name.
+            service_name can be custom or default service name.
+
+
+            :param str service_name: The service name to get GeoLocator.
+
+            :param dict settings:
+
+            :rtype: ``None``, :class:`models.GeoLocator.GeoLocator` or a list of them, if
+                ``exactly_one=False``.
+
+            If the service_name string given is not recognized, a
+            :class:`geopy.exc.GeocoderNotFound` exception is raised.
+            """
+    try:
+        geocoder = get_geocoder_for_service(service_name)
+    except GeocoderNotFound:
+        geocoder = get_custom_geocoder(service_name)
+    return geocoder(user_agent='locator_tester', timeout=6, **settings)
